1a12626a86b5a35fc1a522ad2a29411a18b2570b
Polina Novikova <9104785163@mail.ru>
2021-12-08 10:52:04 +0300

    First edition

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,13 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun printMessage(likes: Int){
+    var endMessage = ""
+    if (likes % 10 == 1) endMessage = "человеку"
+    else if (likes % 10 == 2 || likes % 10 == 3 || likes % 10 == 4) endMessage = "людям"
+    else endMessage = "человекам"
+    println("Понравилось $likes $endMessage")
+}
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    val likes = 65;
+    printMessage(likes)
 }
